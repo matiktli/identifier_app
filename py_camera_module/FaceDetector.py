b4e0bf6e0b62bbda92d6c4695e65a6cd51f3d1b6
matiktli <m.kitlinos@gmail.com>
2019-12-27 19:29:19 +0000

    Some cleaning, making face_detection model more stable

--- py_camera_module/FaceDetector.py
+++ py_camera_module/FaceDetector.py
@@ -6,15 +6,32 @@ class FaceDetector():
     def __init__(self, classifier_path):
         self.face_classifier = cv2.CascadeClassifier(classifier_path)
 
-    def detect_faces(self, frame, num_faces_to_detect=1):
+    def detect_faces(self, frame, num_faces_to_detect=1, min_face_size=100):
         faces = self.face_classifier.detectMultiScale(frame, 1.1, 4)
         if len(faces) == 0:
-            return None
+            return []
 
         result_faces_data = []
         for face in faces[:num_faces_to_detect]:
             x1, y1, w, h = face[0], face[1], face[2], face[3]
             x2, y2 = x1 + w, y1 + h
-            if (x2-x1 > 30):
-                result_faces_data.append((x1, y1, x2, y2))
+            dx, dy = x2 - x1, y2 - y1
+
+            face_data = (x1, y1, x2, y2, dx, dy)
+            if (self.__is_face_allowed(face_data, result_faces_data)):
+                result_faces_data.append(face_data)
+
+        result_faces_data = self.__sort_faces_by_size_desc(result_faces_data)
         return result_faces_data
+
+    def __sort_faces_by_size_desc(self, faces):
+        faces.sort(key=lambda face: face[4] * face[5], reverse=True)
+        return faces
+
+    def __is_face_allowed(self, new_face, existing_faces):
+        (x1, y1, x2, y2, dx, dy) = new_face
+
+        if (dx < 100 or dy < 100):
+            return False
+
+        return True
--- py_identifier/Identifier.py
+++ py_identifier/Identifier.py
@@ -12,15 +12,19 @@ class Identifier():
         self.predition_model = prediction_model
         self.student_data = student_data
 
-    def predict_student_from_frame(self, frame):
-        faces_data = self.face_detector.detect_faces(frame, 1)
+    def predict_student_from_frame(self, frame, faces_to_predict=5):
+        faces_data = self.face_detector.detect_faces(frame, faces_to_predict)
         if faces_data == None:
-            return -1, [], 0
-        valid_frame = self.__extract_valid_frame_for_prediction(
-            frame, faces_data[0])
-        predicted_student_id, confidence = self.__make_prediction_for_student_id(
-            valid_frame)
-        return predicted_student_id, faces_data[0], confidence
+            return [(- 1, [], 0)]
+
+        result = []
+        for face_data in faces_data:
+            valid_frame = self.__extract_valid_frame_for_prediction(
+                frame, face_data)
+            predicted_student_id, confidence = self.__make_prediction_for_student_id(
+                valid_frame)
+            result.append((predicted_student_id, face_data, confidence))
+        return result
 
     def __extract_valid_frame_for_prediction(self, frame, face_data):
         try:
@@ -40,19 +44,19 @@ class Identifier():
             predictions == confidence)[1][0]
         return predicted_student_id, confidence
 
-    def attach_info_to_frame(self, frame, student_id, face_data, confidence):
+    def attach_info_to_frame(self, frame, student_id, face_data, confidence, i=0):
         student_name = self.__get_student_name(student_id)
-        text = student_name + ' - ' + str(confidence)
+        text = f'({str(i)})_ {student_name} : {str(confidence)}'
         cv2.rectangle(
             frame, (face_data[0], face_data[1]), (face_data[2], face_data[3]), (255, 0, 0), 2)
         cv2.putText(frame, text, (face_data[0] + 20, face_data[1] + 20),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        print(f'I am now looking at: {text}')
-        frame = format_frame_by_scale(frame, 0.5)
-        cv2.imshow('I SEE YOU', frame)
+        print(f'{text}')
         return frame
 
     def __get_student_name(self, student_id):
+        if student_id == -1:
+            return '<UNKNOWN>'
         for student in self.student_data:
             if int(student['id']) == int(student_id):
                 return student['name']
@@ -68,9 +72,11 @@ class CameraIdentifierStrategy():
     def start_identification(self):
         while(True):
             frame = self.video_source.read_frame()
-            student_id, face_data, confidence = self.identifier.predict_student_from_frame(
+            data = self.identifier.predict_student_from_frame(
                 frame)
-
-            if student_id != -1:
-                frame = self.identifier.attach_info_to_frame(
-                    frame, student_id, face_data, confidence)
+            for i, (student_id, face_data, confidence) in enumerate(data):
+                if student_id != -1:
+                    frame = self.identifier.attach_info_to_frame(
+                        frame, student_id, face_data, confidence, i)
+            frame = format_frame_by_scale(frame, 0.7)
+            cv2.imshow('I CAN SEE YOU', frame)
